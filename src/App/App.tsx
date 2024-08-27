3aa7c2c15f163254a9be3ebc872f8c21c6e52ccd
TakhirovBizhan <bijantakhirov2004@mail.ru>
2024-08-27 17:42:22 +0300

    new: add to app routing( a bit). new: created a header compoment

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,20 @@
-
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import Header from '../Components/Header' 
 
-function App() {
+import Users from './Users';
+import User from './User';
 
-  return (
-    <>
-    <h1>smth</h1>
-    </>
-  )
-}
+const App = () => {   
+  return (     
+    <BrowserRouter>
+    <Header />         
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/user" element={<User />} />
+      </Routes>    
+    </BrowserRouter>   
+  ); 
+};
 
 export default App
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.module.scss';
+
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
