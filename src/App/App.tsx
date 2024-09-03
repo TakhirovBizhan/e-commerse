87e64c745820898cb8a2f919ecdfe6c68a5ec4bb
TakhirovBizhan <bijantakhirov2004@mail.ru>
2024-09-03 11:03:09 +0300

    fix: installed packages to run project

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,19 +1,15 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Header from '../Components/Header' 
+import Button from '../Components/Button';
 
-import Users from './Users';
-import User from './User';
+
 
 const App = () => {   
   return (     
-    <BrowserRouter>
-    <Header />         
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/user" element={<User />} />
-      </Routes>    
-    </BrowserRouter>   
+    <>
+    <Header />
+    <Button />
+    </>
   ); 
 };
 
