cb0d91a2d98be139514dc96a90f7a37dee04f2ed
TakhirovBizhan <bijantakhirov2004@mail.ru>
2024-09-05 17:13:47 +0300

    new: added two new variations of color for background. Create Notfound file. fix: fixed links.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,23 +6,25 @@ import Categories from '../Pages/Categories';
 import About_us from '../Pages/About_us';
 import Favorites from '../Pages/Favorites';
 import Profile from '../Pages/Profile';
-
-
+import NotFound from '../Pages/NotFound';
 
 
 
 const App = () => {
   return (
-    <BrowserRouter>   
-      <Header />    
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Categories" element={<Categories />} />
-        <Route path='/About_us' element={<About_us />} />
-        <Route path="/Favorites" element={<Favorites />} />
-        <Route path='/Profile' element={<Profile />} />
-      </Routes>    
-    </BrowserRouter>   
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Categories" element={<Categories />} />
+          <Route path='/About_us' element={<About_us />} />
+          <Route path="/Favorites" element={<Favorites />} />
+          <Route path='/Profile' element={<Profile />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </main >
+    </BrowserRouter>
   );
 };
 
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,15 @@
+import Text from "../../Components/Text";
+import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className={styles.main__container}>
+        <Text tag="h2" view="title" color="primary">Products</Text>
+        <Text view="p-20" color="secondary" className={styles.main__container__text}>We display products based on the latest products we have, if you want
+        to see our old products please enter the name of the item</Text>
+      </div>
+    </div>
   )
 }
 
