1573c388986f51447898ff9208704f97fb4d2a57
TakhirovBizhan <bijantakhirov2004@mail.ru>
2024-09-06 21:00:39 +0300

    new: axios requst + 2 interfaces for data and category

--- src/Features/Product_list/Product_list.tsx
+++ src/Features/Product_list/Product_list.tsx
@@ -1,7 +1,45 @@
 import styles from './Product_list.module.css'
 import Text from '../../Components/Text';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 export const Product_list = () => {
+
+  interface ICategory {
+    id: number,
+    name: string,
+    image: string,
+    creationAt: Date,
+    updatedAt: Date
+  }
+
+  interface IData {
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    images: string[],
+    category: ICategory,
+    creationAt: string,
+    updatedAt: string
+
+  }
+
+  useEffect(() => {
+    const fetch = async () => {
+      const resultReq = await axios({
+        method: 'get',
+        url: 'https://api.escuelajs.co/api/v1/products'
+      });
+
+      const result: IData = resultReq.data;
+
+      console.log('result', result);
+    }
+  
+    fetch();
+  }, []);
+
   return (
     <div className={styles.container}>
         <div className={styles.count__block}>
