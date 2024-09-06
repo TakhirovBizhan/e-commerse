a67f24a3b0f4525aa61661437885b0d8b06e61e3
TakhirovBizhan <bijantakhirov2004@mail.ru>
2024-09-06 20:13:22 +0300

    new: title and subtletext for categories and about_us pages

--- src/Pages/About_us/About_us.tsx
+++ src/Pages/About_us/About_us.tsx
@@ -1,7 +1,15 @@
 
+import styles from './About_us.module.css'
+import Text from "../../Components/Text";
+
 export const About_us = () => {
   return (
-    <div>About_us</div>
+    <div>
+      <div className={styles.text__container}>
+        <Text tag="h2" view="title" color="primary">About us</Text>
+        <Text view="p-20" color="secondary" className={styles.text__item}>We are selling furniture!</Text>
+      </div>
+    </div>
   )
 }
 
--- src/Pages/Categories/Categories.tsx
+++ src/Pages/Categories/Categories.tsx
@@ -1,7 +1,13 @@
+import styles from './Categories.module.css'
+import Text from "../../Components/Text";
 
 export const Categories = () => {
   return (
-    <div>Categories</div>
+    <div>
+    <div className={styles.text__container}>
+      <Text tag="h2" view="title" color="primary">Categories</Text>
+    </div>
+  </div>
   )
 }
 export default Categories;
